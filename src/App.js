08cd90ee15cd4f8184b2580f39c82e03b8aa34bf
ts-melo <ts.melo@unifesp.br>
2024-10-23 00:00:41 -0300

    first commit of web app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Header title="novo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header(par){
+    return(
+        <header>
+            <h1>{par.title}</h1>
+
+        </header>     
+    );
+}
+
+export default Header;
